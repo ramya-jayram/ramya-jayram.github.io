99a90a7fda109ca6d6a39cdeededaa3eee7cf00a
Ramya <41500754+ramya-jayram@users.noreply.github.com>
2020-04-29 18:58:03 +0530

    Home page updated

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
 var imageaddress = "https://ramya-jayram.github.io/image";
 
+
 function modefunction(){
     var modechange = document.getElementById('modechange');
     console.log(modechange.src);
 
     if(modechange.src == `${imageaddress}/moon.svg`)
+    // 
     {
         document.body.style.backgroundColor = "#000000";
         modechange.src = `${imageaddress}/sun.svg`;
 
         var navbar = document.getElementById("topbar");
-        navbar.style.background ="#000000"
+        navbar.style.background ="rgba(0, 0, 0, 0.5)0"
 
         // var logoicon = document.getElementById("logoIcon");
         // logoicon.style.fill="#ffffff"
@@ -19,12 +21,19 @@ function modefunction(){
         document.getElementById("logoIcon").style.fill="white";
 
         document.body.style.color="#ffffff";
+
+        // var caretdown = document.getElementById("caretdown");
+        // caretdown.src = `${imageaddress}/caretwhite.svg`;
+
+        document.getElementById("caretdown").style.fill="white";
+        
     } 
     // 
     else
     {
         document.body.style.backgroundColor = "#ffffff";
         modechange.src = `${imageaddress}/moon.svg`;
+
 
         var navbar = document.getElementById("topbar");
         navbar.style.background ="#ffffff"
@@ -36,10 +45,12 @@ function modefunction(){
 
         document.body.style.color="#000000";
 
-        
+        document.getElementById("caretdown").style.fill="#000000";
+
+        // var caretdown = document.getElementById("caretdown");
+        // caretdown.src = `${imageaddress}/caretdark.svg`;
 
     }
 
-    
-
 }
+
