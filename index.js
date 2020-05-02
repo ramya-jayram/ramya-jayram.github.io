1f96ac404806c88157342f840413cc155565f784
Ramya <41500754+ramya-jayram@users.noreply.github.com>
2020-05-02 18:18:07 +0530

    Updated the Design

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function modefunction(){
     var modechange = document.getElementById('modechange');
     console.log(modechange.src);
 
+    //  Dark mode
+
     if(modechange.src == `${imageaddress}/moon.svg`)
-    // 
     {
         document.body.style.backgroundColor = "#000000";
         modechange.src = `${imageaddress}/sun.svg`;
@@ -15,31 +16,31 @@ function modefunction(){
         var navbar = document.getElementById("topbar");
         navbar.style.background ="rgba(0, 0, 0, 0.5)0"
 
-        // var logoicon = document.getElementById("logoIcon");
-        // logoicon.style.fill="#ffffff"
-
         document.getElementById("logoIcon").style.fill="white";
 
         document.body.style.color="#ffffff";
 
-        // var caretdown = document.getElementById("caretdown");
-        // caretdown.src = `${imageaddress}/caretwhite.svg`;
-
         document.getElementById("caretdown").style.fill="white";
+
+        document.getElementById("about").style.background="#191919";
+
+        var card = document.querySelectorAll(".card");
+        for (var i=0; i<card.length; i++) 
+        {
+            card[i].style.background = "#191919";
+            card[i].className = card[i].className + " newclass";
+        }
         
     } 
-    // 
+    //  Day Mode
+
     else
     {
         document.body.style.backgroundColor = "#ffffff";
         modechange.src = `${imageaddress}/moon.svg`;
 
-
         var navbar = document.getElementById("topbar");
-        navbar.style.background ="#ffffff"
-
-        // var logoicon = document.getElementById("logoIcon");
-        // logoicon.style.fill="#000000"
+        navbar.style.background ="rgba(255, 255, 255, 0.5)0"
 
         document.getElementById("logoIcon").style.fill="#000000";
 
@@ -47,10 +48,15 @@ function modefunction(){
 
         document.getElementById("caretdown").style.fill="#000000";
 
-        // var caretdown = document.getElementById("caretdown");
-        // caretdown.src = `${imageaddress}/caretdark.svg`;
+        document.getElementById("about").style.background="#f9f9fa";    
+
+        var card = document.querySelectorAll(".card");
+        for (var i=0; i<card.length; i++) 
+        {
+            card[i].style.background = "#fcfcfc";
+            card[i].className = card[i].className + " newclass";
+        }
 
     }
 
 }
-
